7245901e4619dc9ce4489b3030adb40dc001f72c
ShubhamCac200 <goswamishubham66@gmail.com>
2025-04-08 15:33:35 +0530

    MyCommit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-about',
-  imports: [MatIcon,MatCardModule,MatButtonModule,MatIconModule],
+  imports: [MatCardModule,MatButtonModule,MatIconModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
